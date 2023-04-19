e3652080f27afa010f2f72625eeceb80966dace1
sulco <tomek.sulkowski@gmail.com>
2023-04-19 23:01:00 +0200

    initial grid

---
+++ src/components/board.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    let cells = [...Array(100)].map((v, i) => i);
+</script>
+
+<main>
+    {#each cells as cell}
+       <div class="cell">{cell}</div>
+    {/each}
+</main>
+
+<style>
+    main {
+        display: grid;
+        --size: 30px;
+        grid-template: repeat(10, var(--size)) / repeat(10, var(--size));
+    }
+
+    .cell {
+        text-align: center;
+        border: 1px solid #eee;
+        margin: 0 0 -1px -1px;
+    }
+</style>
