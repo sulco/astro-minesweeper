3ef714d85019c0e0875d5d80d74d9637b179a0ac
sulco <tomek.sulkowski@gmail.com>
2023-04-21 19:47:22 +0200

    two-dimensional generation logic works nice

--- src/components/board.svelte
+++ src/components/board.svelte
@@ -1,64 +1,48 @@
 <script lang="ts">
-    import { randomNumbers } from "../utils";
-    const size = 10;
-    const nopesCt = 3;
+  import { randomNumbers } from '../utils';
+  import Cell from './cell.svelte';
+  const size = 10;
+  const nopesCt = 10;
 
-    let nopes = randomNumbers(3, size*size - 1);
+  let nopes = randomNumbers(nopesCt, size * size - 1);
 
-    let cells = [...Array(size*size)].map((v, i) => {
-        if (nopes.includes(i)) {
-            return -1;
+  let cells = [];
+  for (let y = 0; y < size; y++) {
+    cells.push([]);
+    for (let x = 0; x < size; x++) {
+      cells[y].push(0);
+    }
+  }
+
+  nopes.forEach((index) => {
+    const [x, y] = [index % size, Math.floor(index / size)];
+    cells[y][x] = -1;
+    for (let y1 = y - 1; y1 <= y + 1; y1++) {
+      if (y1 < 0 || y1 >= size) {
+        continue;
+      }
+      for (let x1 = x - 1; x1 <= x + 1; x1++) {
+        if (x1 < 0 || x1 >= size || nopes.includes(y1 * size + x1)) {
+          continue;
         }
-        return 0;
-    });
-
-    nopes.forEach(v => {
-        for (let x = -1; x <= 1; x++) {
-            for (let y = -1; y <= 1; y++) {
-                const idx = v + size * y + x;
-                if (nopes.includes(idx)) {
-                    continue;
-                }
-                cells[idx] += 1;
-            }
-        }
-    });
+        cells[y1][x1]++;
+      }
+    }
+  });
 </script>
 
 <main>
-    {#each cells as cell}
-        {#if !cell}
-            <div class="cell"></div>
-        {/if}
-        {#if cell}
-            <div class="cell" data-number={cell}>{cell > -1 ? cell : '😮'}</div>
-        {/if}
+  {#each cells as row}
+    {#each row as cell}
+      <Cell value={cell} />
     {/each}
+  {/each}
 </main>
 
 <style>
-    main {
-        display: grid;
-        --size: 40px;
-        grid-template: repeat(10, var(--size)) / repeat(10, var(--size));
-    }
-
-    .cell {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border: 1px solid #eee;
-        margin: 0 0 -1px -1px;
-        position: relative;
-    }
-
-    /* .cell::before {
-        content: attr(data-number);
-        position: absolute;
-        top: 0px;
-        left: 1px;
-        font-size: 11px;
-        background: white;
-        padding: 1px;
-    } */
+  main {
+    display: grid;
+    --size: 40px;
+    grid-template: repeat(10, var(--size)) / repeat(10, var(--size));
+  }
 </style>
---
+++ src/components/cell.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let value = 0;
+</script>
+
+<div class="cell" data-number={value}>
+  {#if value === -1}
+    😮
+  {:else if value > 0}
+    {value}
+  {/if}
+</div>
+
+<style>
+  .cell {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #eee;
+    margin: 0 0 -1px -1px;
+    position: relative;
+  }
+</style>
